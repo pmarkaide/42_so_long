d0a3b9c87af64b98c1f93405fcd951ce87dbfef1
Pablo Markaide <pablo.markaide@gmail.com>
2024-01-20 11:54:41 +0200

    chore: map rename; wip functions

--- flood_fill.c
+++ flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 17:15:01 by pmarkaid          #+#    #+#             */
-/*   Updated: 2024/01/19 17:30:21 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/01/20 11:30:57 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,7 @@ int	is_valid_path(int x, int y, t_data *data)
 void	flood_fill(int x, int y, t_data *data)
 {
 	if (!is_valid_path(x, y, data))
-	{
-		return ;
-	}
+		return;
 	data->visited[x][y] = 1;
 	flood_fill(x - 1, y, data);
 	flood_fill(x + 1, y, data);
@@ -40,3 +38,35 @@ void	flood_fill(int x, int y, t_data *data)
 
 // starting position calculation
 // check C and E against visited
+
+int map_has_valid_path(t_data *data)
+{
+int startX, startY; // Identify the starting point (player position)
+
+    // Find the starting position 'P'
+    for (int i = 0; i < data->rows; i++) {
+        for (int j = 0; j < data->cols; j++) {
+            if (data->map[i][j] == 'P') {
+                startX = i;
+                startY = j;
+                break;
+            }
+        }
+    }
+
+    floodFill(startX, startY);
+
+    // Check if all collectibles and exit are visited
+    for (int i = 0; i < data->rows; i++) {
+        for (int j = 0; j < data->cols; j++) {
+            if ((data->map[i][j] == 'C' || data->map[i][j] == 'E') && !visited[i][j]) {
+                printf("Invalid path!\n");
+                return 0;
+            }
+        }
+    }
+
+    printf("Valid path exists!\n");
+
+    return 0;
+}
