c2bf632b21b6acf531bcc737428f638d4e11155b
Pablo Markaide <pmarkaid@student.hive.fi>
2024-01-23 15:22:30 +0200

    feat: pacman moves, with a trace

--- src/game_init.c
+++ src/game_init.c
@@ -1,6 +1,15 @@
 #include "../include/so_long.h"
 
 static mlx_image_t	*g_img;
+static int32_t		g_pos_x = 0;
+static int32_t		g_pos_y = 0;
+
+
+static void error(void)
+{
+	puts(mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
 
 void	hook(void* param)
 {
@@ -9,8 +18,17 @@ void	hook(void* param)
 	mlx = param;
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
-	if (mlx_is_key_down(mlx, MLX_KEY_P))
-		mlx_delete_image(mlx, g_img);
+
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		g_pos_y -= 10;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		g_pos_y += 10;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		g_pos_x -= 10;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		g_pos_x += 10;
+
+	mlx_image_to_window(mlx, g_img, g_pos_x, g_pos_y);
 } 
 
 int32_t	game_init(void)
@@ -20,8 +38,22 @@ int32_t	game_init(void)
 	mlx = mlx_init(WIDTH, HEIGHT, "cool game", true);
 	if (!mlx)
 		exit(EXIT_FAILURE);
-	g_img = mlx_new_image(mlx, WIDTH, HEIGHT);
-	mlx_image_to_window(mlx, g_img, 0, 0);
+
+	mlx_texture_t* texture = mlx_load_png("textures/480px-Pacman_HD.png");
+	if (!texture)
+        error();
+
+	// Convert texture to a displayable image
+	g_img = mlx_texture_to_image(mlx, texture);
+	if (!g_img)
+        error();
+
+	// Set the initial position of the image
+	g_pos_x = 200;
+	g_pos_y = 200;
+
+	// Display the image
+	mlx_image_to_window(mlx, g_img, g_pos_x, g_pos_y);
 	mlx_loop_hook(mlx, &hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
